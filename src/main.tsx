2df5ef2e547ee9aa9f4424c18b55b1ca450e7eab
tompra <tomprayon@gmail.com>
2024-04-03 16:07:11 +0200

    app context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
 import './index.css';
+import { AppProvider } from './context/context.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <AppProvider>
+        <App />
+    </AppProvider>
+);
