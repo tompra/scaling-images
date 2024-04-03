5d8f552f20da93f83c2821dc4da994b4210703e1
tompra <tomprayon@gmail.com>
2024-04-03 15:48:33 +0200

    render components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,14 @@
-const App = () => {
-    return <div>App</div>;
+import Gallery from './Gallery';
+import Search from './Search';
+import ThemeToggle from './ThemeToggle';
+
+const App: React.FC = (): JSX.Element => {
+    return (
+        <main>
+            <ThemeToggle />
+            <Search />
+            <Gallery />
+        </main>
+    );
 };
 export default App;
