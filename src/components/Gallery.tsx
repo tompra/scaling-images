6841fd7ae4739dbbde440de19fa787027ff6297f
tompra <tomprayon@gmail.com>
2024-04-04 17:31:26 +0200

    manage status from data retrieve and display data

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,53 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { ImagesResponseData } from '../types';
+import { useSearchContext } from '../context/SearchContext';
+import { ACCESS_KEY } from '../../secrets.json';
+
+const API_URL = `https://api.unsplash.com/search/photos?client_id=${ACCESS_KEY}&query=`;
+
 const Gallery = () => {
-    return <div>Gallery</div>;
+    const { searchTerm } = useSearchContext();
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['images', searchTerm],
+        queryFn: async () => {
+            const result = await axios.get(`${API_URL}${searchTerm}`);
+            return result.data;
+        },
+    });
+
+    if (isLoading) {
+        return (
+            <section className='images-container'>
+                <h1>Loading...</h1>
+            </section>
+        );
+    }
+
+    if (error) {
+        return (
+            <section className='images-container'>
+                <h1>{`An error has occurred: ${error?.message}`}</h1>
+            </section>
+        );
+    }
+
+    if (data.results.length < 1) {
+        return (
+            <section className='images-container'>
+                <h1>No results are found</h1>
+            </section>
+        );
+    }
+
+    return (
+        <section className='images-container'>
+            {data.results.map((item: ImagesResponseData) => {
+                console.log('item', item);
+                const url = item?.urls?.regular;
+                return <img key={item.id} src={url} />;
+            })}
+        </section>
+    );
 };
 export default Gallery;
