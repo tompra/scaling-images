14ec480320bf486aae95b12a1e7e3e58233ed8ca
tompra <tomprayon@gmail.com>
2024-04-04 17:29:21 +0200

    create search context

---
+++ src/context/SearchContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from 'react';
+import { ContextProviderProps, SearchContextType } from '../types';
+
+const SearchContext = createContext<SearchContextType>({
+    searchTerm: 'coffee',
+    setSearchTerm: () => {},
+});
+
+export const SearchProvider: React.FC<ContextProviderProps> = ({
+    children,
+}) => {
+    const [searchTerm, setSearchTerm] = useState('coffee');
+    return (
+        <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+            {children}
+        </SearchContext.Provider>
+    );
+};
+
+export const useSearchContext = () => useContext(SearchContext);
