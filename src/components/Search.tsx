b18164f9b92e2e5456b9f4215de14867dfc22e2e
tompra <tomprayon@gmail.com>
2024-04-04 15:23:34 +0200

    search functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,38 @@
+import { ChangeEvent, useState } from 'react';
+
 const Search = () => {
-    return <div>Search</div>;
+    const [inputValue, setInputValue] = useState<string>('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const userInput = e.currentTarget.search.value;
+        if (!userInput) return;
+        console.log(userInput);
+        setInputValue('');
+    };
+
+    const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const userValue = e.target.value;
+        setInputValue(userValue);
+    };
+
+    return (
+        <section className='search--container'>
+            <h1>scaling images</h1>
+            <form className='search--form' onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    name='search'
+                    placeholder='search...'
+                    className='form--input__search'
+                    onChange={onSearchChange}
+                    value={inputValue}
+                />
+                <button type='submit' className='form--btn__search'>
+                    search
+                </button>
+            </form>
+        </section>
+    );
 };
 export default Search;
