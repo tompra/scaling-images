3e9e428bb6dbcfa1c2448ae2b155485c093598f4
tompra <tomprayon@gmail.com>
2024-04-04 17:30:44 +0200

    setup react query and devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
-import { AppProvider } from './context/context.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx';
 import './styles/index.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { SearchProvider } from './context/SearchContext.tsx';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <AppProvider>
-        <App />
-    </AppProvider>
+    <ThemeProvider>
+        <SearchProvider>
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </SearchProvider>
+    </ThemeProvider>
 );
