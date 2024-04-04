a93b164198ecaaa818a91637278e3393f81ab1e0
tompra <tomprayon@gmail.com>
2024-04-04 17:32:12 +0200

    change name function context

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,8 +1,8 @@
-import { useGlobalContext } from '../context/context';
+import { useThemeContext } from '../context/ThemeContext';
 import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
 const ThemeToggle = () => {
-    const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+    const { isDarkTheme, toggleDarkTheme } = useThemeContext();
     return (
         <section className='toggle--btn__container'>
             <button className='toggle--btn__theme' onClick={toggleDarkTheme}>
