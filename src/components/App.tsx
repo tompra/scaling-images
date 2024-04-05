74c946cf85dd0781b5f01e1756058895186a5adc
tompra <tomprayon@gmail.com>
2024-04-05 14:07:59 +0200

    conditional rendering of pagination component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,17 @@
+import useFetchData from '../hook/useFetchData';
 import Gallery from './Gallery';
+import Pagination from './Pagination';
 import Search from './Search';
 import ThemeToggle from './ThemeToggle';
 
 const App: React.FC = (): JSX.Element => {
+    const { isLoading, isFetching, error } = useFetchData();
     return (
         <main>
             <ThemeToggle />
             <Search />
             <Gallery />
+            {isLoading || isFetching || error ? null : <Pagination />}
         </main>
     );
 };
